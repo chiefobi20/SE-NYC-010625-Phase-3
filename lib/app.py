a27345b0ccb9f420a7f4269bebbf79f1101371ca
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-02-20 10:38:14 -0500

    02 lecture updates

--- lib/app.py
+++ lib/app.py
@@ -1,5 +1,40 @@
 import ipdb
 
+# A list of numbers
+numbers_list= [7, 14]
+
+# A tuple of numbers
+numbers_tuple = (8,)
+
+# A tuple that has a string and a boolean
+items = ('pizza', True)
+
+# A list of duplicate numbers
+duplicate_numbers_list = [2,2,3,3,4,4]
+
+# A set that removes the duplicate items from the list
+numbers_set = set(duplicate_numbers_list)
+
+# A list that contains
+numbers_list_without_duplicates = list(numbers_set)
+
+# A string
+phrase = 'hello flatiron'
+
+# A string of dulplicate characters
+fruit = "apple"
+
+# A set that removes the duplicate characters from the "apple" string
+set_of_chars = set(fruit)
+
+# Taking the characters from the set and creating a new string that contains these characters with the duplicates having been removed
+new_string = ""
+for char in set_of_chars:
+    new_string += char
+
+print((f"My favorite phrase is {phrase}"))
+
+
 def combine_sequences(seq1, seq2):
     pass
 
@@ -27,6 +62,9 @@ foods = [
     }
 ]
 
+# Example code that returns a filtered list of the prices that are greater than 3
+filtered_prices_list = [food['price'] for food in foods if food['price'] > 3]
+
 animals = [
     {
         "name": "Fido",
@@ -41,3 +79,5 @@ animals = [
         "animal_type": "Guinea Pig"
     }
 ]
+# ipdb.set_trace is a debugger that stops your code in a certain line(won't be able to add new code)
+ipdb.set_trace()
